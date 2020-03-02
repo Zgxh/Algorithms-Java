541a67333fde9b06de71016e737de40bb36d0456
Zgxh <1227814546@qq.com>
2020-03-02 19:45:56 +0800

    2020-3-2 剑指offer-64：滑动窗口的最大值

--- SyntaxTest/Test.java
+++ SyntaxTest/Test.java
@@ -6,9 +6,8 @@ package SyntaxTest;
 public class Test {
 
     public static void main(String[] args) {
-        int[] result = new int[3];
-        for (int i = 0; i < 3; i++) {
-            System.out.println(result[i]);
-        }
+        String a = "32";
+        String b = "3";
+        System.out.println(a.compareTo(b));
     }
 }
