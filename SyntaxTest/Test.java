21a9c2385689894edd5c2f27597ed97de595cf17
YuYang <1227814546@qq.com>
2020-07-05 15:37:55 +0800

    2020-7-5 [mid] stack

--- SyntaxTest/Test.java
+++ SyntaxTest/Test.java
@@ -6,8 +6,10 @@ package SyntaxTest;
 public class Test {
 
     public static void main(String[] args) {
-        String a = "32";
-        String b = "3";
-        System.out.println(a.compareTo(b));
+        String a = "3:start:4";
+        String b = "2:end:5";
+        String[] c = null;
+        System.out.println((c = a.split("end")).length);
+//        Arrays.stream(a.split("end")).forEach(System.out::println);
     }
 }
