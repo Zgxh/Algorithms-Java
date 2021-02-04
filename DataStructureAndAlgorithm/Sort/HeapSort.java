7d51b741141c0c4a265431fa04ee04cf718c0d08
Zgxh <1227814546@qq.com>
2021-02-04 11:26:45 +0800

    2021-2-4 [hard] leetbook update

--- DataStructureAndAlgorithm/Sort/HeapSort.java
+++ DataStructureAndAlgorithm/Sort/HeapSort.java
@@ -4,6 +4,7 @@ public class HeapSort {
     
     /**
      * 堆排序：采用大顶堆实现元素的非递减序排序。
+     *
      * @param array 存放堆元素的数组
      */
     public static void heapSort(int[] array) {
@@ -13,15 +14,20 @@ public class HeapSort {
         }
         // 利用堆进行排序，每次将堆顶弹出，放到比之前次大的位置
         for (int i = array.length - 1; i > 0; i--) {
+            // 选出当前最大元素，即堆顶元素，交换到堆的末尾
             int temp = array[0];
             array[0] = array[i];
             array[i] = temp;
+            // 重新调整，使堆符合结构
             heapAdjust(array, i, 0);
         }
     }
 
     /**
-     * 堆调整过程。把堆重新调整为大顶堆。
+     * 堆调整：把堆重新调整为大顶堆。
+     * 堆的特点是任意子树的根大于其孩子的值，如果不满足，则选择左右孩子中较大的一个进行上浮，
+     * 并递归实现堆的结构
+     *
      * @param array 存放堆数据的数组
      * @param len 截止的数组长度
      * @param i 当前需要调整的元素所在的位置
@@ -36,7 +42,9 @@ public class HeapSort {
                 }
             }
             if (array[index] > temp) {
+                // 左右孩子中更大的结点上浮
                 array[k] = array[index];
+                // 递归解决由于孩子节点上浮导致的子树不符合堆结构的问题
                 k = index;
                 index = 2 * index + 1;
             } else {
